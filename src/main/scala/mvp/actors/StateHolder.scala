e9580b84ab41ec0c95997789afc8d6432f3e463c
aleksandr <alexbromel@gmail.com>
2018-08-06 17:47:31 +0300

    Remove processor

--- src/main/scala/mvp/actors/StateHolder.scala
+++ src/main/scala/mvp/actors/StateHolder.scala
@@ -2,18 +2,30 @@ package mvp.actors
 
 import akka.actor.Actor
 import mvp.actors.StateHolder.{Headers, Payloads, Transactions}
+import mvp.modifiers.Modifier
 import mvp.modifiers.blockchain.{Header, Payload}
 import mvp.modifiers.mempool.Transaction
 import mvp.view.blockchain.Blockchain
-import mvp.view.blockchain.processor.ModifiersProcessor
 import mvp.view.mempool.Mempool
 import mvp.view.state.State
 
-class StateHolder extends Actor with ModifiersProcessor {
+class StateHolder extends Actor {
 
   var blockChain: Blockchain = Blockchain.recoverBlockchain
   var state: State = State.recoverState
   var mempool: Mempool = Mempool()
+
+  def apply(modifier: Modifier): Unit = modifier match {
+    case header: Header => blockChain = blockChain.addHeader(header)
+    case payload: Payload => blockChain = blockChain.addPayload(payload)
+    case transaction: Transaction => mempool = mempool.put(Seq(transaction))
+  }
+
+  def validate(modifier: Modifier): Boolean = modifier match {
+    case header: Header => ???
+    case payload: Payload => ???
+    case transaction: Transaction => ???
+  }
 
   override def receive: Receive = {
     case Headers(headers: Seq[Header]) => headers.filter(validate).foreach(apply)
