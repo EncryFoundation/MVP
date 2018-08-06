3c0bea7a94a1d6c9dfa9cb0a12765b2830fbeb8c
timofey <gusev.t.s1@gmail.com>
2018-08-06 14:42:36 +0300

    add logger

--- src/main/scala/mvp/actors/Starter.scala
+++ src/main/scala/mvp/actors/Starter.scala
@@ -1,13 +1,14 @@
 package mvp.actors
 
 import akka.actor.{Actor, Props}
+import com.typesafe.scalalogging.StrictLogging
 import mvp.MVP.{settings, system}
 import mvp.actors.Messages.{Heartbeat, Start}
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class Starter extends Actor {
+class Starter extends Actor with StrictLogging{
 
   override def preStart(): Unit = {
     context.system.scheduler.schedule(initialDelay = 10 seconds, interval = settings.heartbeat seconds)(self ! Heartbeat)
@@ -15,13 +16,13 @@ class Starter extends Actor {
 
   override def receive: Receive = {
     case Start if settings.testMode =>
-      println("test mode on starter")
+      logger.info("test mode on starter")
       val networker = context.actorOf(Props[Networker].withDispatcher("net-dispatcher").withMailbox("net-mailbox"), "networker")
       //system.actorSelection("/user/starter/networker") ! Start
       networker ! Start
     case Start if settings.testMode =>
-      println("real life baby on starter")
-    case Heartbeat => println("heartbeat pong")
+      logger.info("real life baby on starter")
+    case Heartbeat => logger.info("heartbeat pong")
     case _ =>
   }
 
