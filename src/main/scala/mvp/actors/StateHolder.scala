2ae90426e9c1661b527c6119e669d2274b8fae5d
Bromel777 <33941049+Bromel777@users.noreply.github.com>
2018-08-09 17:34:05 +0300

    Blocks and messages sharing (#13)
    * Opportunity to send blocks added
    
    * Opportunity to share messages added
    
    * Add logging

--- src/main/scala/mvp/actors/StateHolder.scala
+++ src/main/scala/mvp/actors/StateHolder.scala
@@ -1,28 +1,38 @@
 package mvp.actors
 
 import akka.actor.Actor
-import mvp.actors.StateHolder.{GetLastBlock, Headers, Payloads, Transactions}
-import mvp.local.Keys
+import com.typesafe.scalalogging.StrictLogging
+import io.circe.{Decoder, Encoder, HCursor}
+import io.circe.syntax._
+import mvp.actors.StateHolder._
+import mvp.local.messageHolder.UserMessage
+import mvp.local.messageTransaction.MessageInfo
+import mvp.local.{Generator, Keys}
 import mvp.modifiers.Modifier
-import mvp.modifiers.blockchain.{Header, Payload}
+import mvp.modifiers.blockchain.{Block, Header, Payload}
 import mvp.modifiers.mempool.Transaction
+import mvp.modifiers.state.output.MessageOutput
 import mvp.view.blockchain.Blockchain
 import mvp.view.state.State
 import scorex.crypto.signatures.Curve25519
 
-class StateHolder extends Actor {
+class StateHolder extends Actor with StrictLogging {
 
   var blockChain: Blockchain = Blockchain.recoverBlockchain
   var state: State = State.recoverState
   val keys: Keys = Keys.recoverKeys
+  var messagesHolder: Seq[UserMessage] = Seq.empty
 
   def apply(modifier: Modifier): Unit = modifier match {
     case header: Header =>
+      logger.info(s"Get header: ${Header.jsonEncoder(header)}")
       blockChain = blockChain.addHeader(header)
     case payload: Payload =>
+      logger.info(s"Get payload: ${Payload.jsonEncoder(payload)}")
       blockChain = blockChain.addPayload(payload)
       state = state.updateState(payload)
     case transaction: Transaction =>
+      logger.info(s"Get transaction: ${Transaction.jsonEncoder(transaction)}")
       val payload: Payload = Payload(Seq(transaction))
       val headerUnsigned: Header = Header(
         System.currentTimeMillis(),
@@ -38,11 +48,18 @@ class StateHolder extends Actor {
       self ! Payload
   }
 
+  def addMessage(message: UserMessage, previousMessage: Option[MessageInfo], outputId: Option[Array[Byte]]): Unit =
+    if (!messagesHolder.contains(message)) {
+      logger.info(s"Get message: ${UserMessage.jsonEncoder(message)}")
+      messagesHolder = messagesHolder :+ message
+      apply(Generator.generateMessageTx( keys.keys.head, previousMessage, outputId, message.message))
+    }
+
   def validate(modifier: Modifier): Boolean = modifier match {
     //TODO: Add semantic validation check
     case header: Header =>
-      blockChain.getHeaderAtHeight(header.height - 1)
-        .exists(prevHeader => header.previousBlockHash sameElements prevHeader.id) || header.height == 0
+       header.height == 0 || (header.height > blockChain.headers.last.height && blockChain.getHeaderAtHeight(header.height - 1)
+         .exists(prevHeader => header.previousBlockHash sameElements prevHeader.id))
     case payload: Payload =>
       payload.transactions.forall(validate)
     case transaction: Transaction =>
@@ -54,13 +71,25 @@ class StateHolder extends Actor {
 
   override def receive: Receive = {
     case Headers(headers: Seq[Header]) => headers.filter(validate).foreach(apply)
+    case Message(msg: UserMessage) =>
+      val previousMessageInfo: Option[MessageInfo] =
+        msg.prevOutputId.flatMap( outputId => state
+          .state
+          .get(outputId)
+          .map(_.asInstanceOf[MessageOutput].toMessageInfo(msg.message)))
+      addMessage(msg, previousMessageInfo, msg.prevOutputId)
     case Payloads(payloads: Seq[Payload]) => payloads.filter(validate).foreach(apply)
     case Transactions(transactions: Seq[Transaction]) => transactions.filter(validate).foreach(apply)
     case GetLastBlock => sender() ! blockChain.blocks.last
+    case GetLastInfo => sender() ! LastInfo(blockChain.blocks, messagesHolder)
   }
 }
 
 object StateHolder {
+
+  case object GetLastInfo
+
+  case class Message(msg: UserMessage)
 
   case object GetLastBlock
 
@@ -69,4 +98,22 @@ object StateHolder {
   case class Payloads(payloads: Seq[Payload])
 
   case class Transactions(transaction: Seq[Transaction])
+
+  case class LastInfo(blocks: Seq[Block], messages: Seq[UserMessage])
+
+  object LastInfo {
+
+    implicit val jsonDecoder: Decoder[LastInfo] = (c: HCursor) => for {
+      blocks <- c.downField("blocks").as[Seq[Block]]
+      messages <- c.downField("proof").as[Seq[UserMessage]]
+    } yield LastInfo(
+      blocks,
+      messages
+    )
+
+    implicit val jsonEncoder: Encoder[LastInfo] = (b: LastInfo) => Map(
+      "blocks" -> b.blocks.map(_.asJson).asJson,
+      "messages" -> b.messages.map(_.asJson).asJson
+    ).asJson
+  }
 }
